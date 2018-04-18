7fd3c471fa4c8914bc5d4a0ec3467dbeb6d6bb36
wangbowen <xiaowen1314jun../>
2018-04-18 15:31:11 +0800

    新增网关zuul服务，对外提供访问的统一入口

---
+++ ares-api-gateway/src/main/java/com/study/cloud/aresapigateway/AresApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.study.cloud.aresapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AresApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AresApiGatewayApplication.class, args);
+	}
+}
---
+++ ares-api-gateway/src/test/java/com/study/cloud/aresapigateway/AresApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.study.cloud.aresapigateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AresApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
