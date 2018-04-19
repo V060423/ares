f3b4df0dc6aaee5efd6342b85dc0e79efaf1ed47
wangbowen <xiaowen1314jun../>
2018-04-19 16:15:04 +0800

    调整方法

--- ares-service-consumer/src/main/java/AresServiceConsumerApplication.java
+++ ares-service-consumer/src/main/java/AresServiceConsumerApplication.java
@@ -1,9 +1,10 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients({"com.study.cloud.service"})
 @SpringBootApplication(scanBasePackages = {
 		"com.study.cloud.web",
