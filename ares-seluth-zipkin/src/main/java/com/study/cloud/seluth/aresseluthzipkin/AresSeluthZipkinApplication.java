980bb7d31e03eddeb3d1520a1c81fbe15712f40d
wangbowen <xiaowen1314jun../>
2018-04-25 15:19:38 +0800

    新增服务链路跟踪服务

---
+++ ares-seluth-zipkin/src/main/java/com/study/cloud/seluth/aresseluthzipkin/AresSeluthZipkinApplication.java
@@ -0,0 +1,12 @@
+package com.study.cloud.seluth.aresseluthzipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AresSeluthZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AresSeluthZipkinApplication.class, args);
+	}
+}
---
+++ ares-seluth-zipkin/src/test/java/com/study/cloud/seluth/aresseluthzipkin/AresSeluthZipkinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.study.cloud.seluth.aresseluthzipkin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AresSeluthZipkinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
